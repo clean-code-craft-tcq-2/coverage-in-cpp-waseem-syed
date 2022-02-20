f8cc1e141ba9ce81c77fdf42c6575f2e0d9e7f24
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-02-20 17:55:16 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -10,3 +10,15 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(25, 20, 30) == NORMAL);
   REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
 }
+
+TEST_CASE("verify limits based on given cooling type")
+{
+	CoolingTypeAndTemperatureLimits coolingTypePassiveAndTemperatureLimits(PASSIVE_COOLING, 0, 35);
+	REQUIRE(getTemperatureLimitsForCoolingType(PASSIVE_COOLING) == coolingTypePassiveAndTemperatureLimits);
+	
+  CoolingTypeAndTemperatureLimits coolingTypeHIActiveAndTemperatureLimits(HI_ACTIVE_COOLING, 0, 45);
+	REQUIRE(getTemperatureLimitsForCoolingType(HI_ACTIVE_COOLING) == coolingTypeHIActiveAndTemperatureLimits);
+	
+	CoolingTypeAndTemperatureLimits coolingTypeMedActiveAndTemperatureLimits(MED_ACTIVE_COOLING, 0, 40);
+	REQUIRE(getTemperatureLimitsForCoolingType(MED_ACTIVE_COOLING) == coolingTypeMedActiveAndTemperatureLimits);
+}
