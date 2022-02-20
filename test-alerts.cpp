c2a3cc433923d6d7a8450ce1f17e23e42acb23f3
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-02-20 18:05:42 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -13,12 +13,23 @@ TEST_CASE("infers the breach according to limits") {
 
 TEST_CASE("verify limits based on given cooling type")
 {
-	CoolingTypeAndTemperatureLimits coolingTypePassiveAndTemperatureLimits(PASSIVE_COOLING, 0, 35);
-	REQUIRE(getTemperatureLimitsForCoolingType(PASSIVE_COOLING) == coolingTypePassiveAndTemperatureLimits);
+	CoolingTypeAndTemperatureLimits coolingTypeAndTemperatureLimitsResults;
 	
-  CoolingTypeAndTemperatureLimits coolingTypeHIActiveAndTemperatureLimits(HI_ACTIVE_COOLING, 0, 45);
-	REQUIRE(getTemperatureLimitsForCoolingType(HI_ACTIVE_COOLING) == coolingTypeHIActiveAndTemperatureLimits);
+	CoolingTypeAndTemperatureLimits coolingTypePassiveAndTemperatureLimits(PASSIVE_COOLING, 0, 35);
+	coolingTypeAndTemperatureLimitsResults = getTemperatureLimitsForCoolingType(PASSIVE_COOLING);
+	REQUIRE(coolingTypeAndTemperatureLimitsResults.m_coolingType == coolingTypePassiveAndTemperatureLimits.m_coolingType);
+	REQUIRE(coolingTypeAndTemperatureLimitsResults.m_lowerLimit == coolingTypePassiveAndTemperatureLimits.m_lowerLimit);
+	REQUIRE(coolingTypeAndTemperatureLimitsResults.m_upperLimit == coolingTypePassiveAndTemperatureLimits.m_upperLimit);
+	
+        CoolingTypeAndTemperatureLimits coolingTypeHIActiveAndTemperatureLimits(HI_ACTIVE_COOLING, 0, 45);
+	coolingTypeAndTemperatureLimitsResults = getTemperatureLimitsForCoolingType(HI_ACTIVE_COOLING);
+	REQUIRE(coolingTypeAndTemperatureLimitsResults.m_coolingType == coolingTypeHIActiveAndTemperatureLimits.m_coolingType);
+	REQUIRE(coolingTypeAndTemperatureLimitsResults.m_lowerLimit == coolingTypeHIActiveAndTemperatureLimits.m_lowerLimit);
+	REQUIRE(coolingTypeAndTemperatureLimitsResults.m_upperLimit == coolingTypeHIActiveAndTemperatureLimits.m_upperLimit);
 	
 	CoolingTypeAndTemperatureLimits coolingTypeMedActiveAndTemperatureLimits(MED_ACTIVE_COOLING, 0, 40);
-	REQUIRE(getTemperatureLimitsForCoolingType(MED_ACTIVE_COOLING) == coolingTypeMedActiveAndTemperatureLimits);
+	coolingTypeAndTemperatureLimitsResults = getTemperatureLimitsForCoolingType(MED_ACTIVE_COOLING) ;
+	REQUIRE(coolingTypeAndTemperatureLimitsResults.m_coolingType == coolingTypeMedActiveAndTemperatureLimits.m_coolingType);
+	REQUIRE(coolingTypeAndTemperatureLimitsResults.m_lowerLimit == coolingTypeMedActiveAndTemperatureLimits.m_lowerLimit);
+	REQUIRE(coolingTypeAndTemperatureLimitsResults.m_upperLimit == coolingTypeMedActiveAndTemperatureLimits.m_upperLimit);
 }
