20e8e41ff5fc2ec24ed45d0d9af398597c1ae7c5
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-02-21 19:29:33 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -61,22 +61,6 @@ void sendToController(BreachType breachType) {
   printf("%x : %x\n", header, breachType);
 }
 
-void sendToEmail(BreachType breachType) {
-  const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
-  }
-}
-
 void printBreachType(BreachType breachType)
 {
    const char* recepient = "a.b@c.com";
